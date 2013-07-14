22cd44268276e255aabac5a133b6e7857d14a077
Jeff Blaisdell <jeff.blaisdell@gmail.com>
2013-07-14 11:03:32 -0500

    Added post view page.

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from blogs import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<post_id>\d)/$', views.view_post, name='post')
 )
--- blogs/views.py
+++ blogs/views.py
@@ -4,3 +4,7 @@ from blogs.models import Post
 def index(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blogs/index.html', context)
+
+def view_post(request, post_id):
+    context = {'post': Post.objects.get(id=post_id) }
+    return render(request, 'blogs/post.html', context)
