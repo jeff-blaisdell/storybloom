16790590d1e8e566182640dcb78cae5b758e1bfb
Jessica Blaisdell <jessheuv@gmail.com>
2013-09-27 16:37:07 -0500

    scrolltop

--- storybloom/static/scripts/scrolltop.js
+++ storybloom/static/scripts/scrolltop.js
@@ -1,9 +1,17 @@
 
+	
+	$(window).scroll(function(){
+        if ($(this).scrollTop() > 110) {
+            $('.top').fadeIn();
+        } else {
+            $('.top').fadeOut();
+        }
+    });
+
 	$(document).ready(function() {
 		$('.top').click(function() {
-			$('html, body').animate({ scrollTop: 0 }, 'slow');
+			$('html, body').animate({ scrollTop: 0 }, 'medium');
 		});
 	});
 	
 
-	
